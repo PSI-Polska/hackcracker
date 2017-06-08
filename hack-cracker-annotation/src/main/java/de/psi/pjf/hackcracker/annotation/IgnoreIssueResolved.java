7c1dedc324f703768c3c5d71d1ceda71044985ea
psi\akedziora <psi\akedziora@5521faae-5cfa-d344-9c80-64878d874c08>
2017-06-08 10:49:40 +0000

    additional javadocs
    git-svn-id: https://com-bld-svn.psi.de/svn/incubator/hackcracker/trunk@1977 5521faae-5cfa-d344-9c80-64878d874c08

--- hack-cracker-annotation/src/main/java/de/psi/pjf/hackcracker/annotation/IgnoreIssueResolved.java
+++ hack-cracker-annotation/src/main/java/de/psi/pjf/hackcracker/annotation/IgnoreIssueResolved.java
@@ -22,9 +22,33 @@ import java.lang.annotation.Target;
 @Retention(SOURCE)
 public @interface IgnoreIssueResolved
 {
+    /**
+     * Version of component that your project is using. 
+     * Maybe in the future this will be somehow automatically resolved.
+     * @return version of dependency used by project.
+     */
     String versionUsed();
     
+    /**
+     * Version of component that the fix is for. 
+     * The idea is that this annotation is placed only after the issue actually 
+     * has been resolved and one cannot upgrade to the newer version for some 
+     * reason so the version is known.
+     * Maybe in the future this will be somehow automatically resolved.
+     * @return version of dependency that the fix is for.
+     */
     String versionResolved();
     
+    /**
+     * Short description of why the component cannot be upgraded to the version 
+     * where the bug is not present and thus the hac one made being not necessary.
+     * The idea is that this annotation is placed only after the issue actually 
+     * has been resolved and one cannot upgrade to the newer version for some 
+     * reason. A meaningful explanation of why it is the case should be provided
+     * and currently the algorithm of determining if the explanation is meaningful
+     * is simply - is it at least 128 characters long. This is mainly so that 
+     * ignoring cannot be done out of habit or at least that is authors hope.
+     * @return a meaningful explanation.
+     */
     String reasoningWhyHackCannotBeRemoved();
 }
